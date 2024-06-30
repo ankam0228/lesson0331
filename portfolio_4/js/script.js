393231cb2e10374af00cee71f62a805a43f12b53
ankam0228 <162595831+ankam0228@users.noreply.github.com>
2024-06-30 15:13:53 +0900

    Add files via upload

---
+++ portfolio_4/js/script.js
@@ -0,0 +1,35 @@
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+
+  // クリック後「disply:block;」でメニューが表示される
+  // #burgerが非表示になる
+  $('#burger').on('click', function(){
+    $('#nav-open').css('display','block');
+    $('#burger').css('display','none');
+  });
+  // クリック後「disply:none;」でメニューが削除される
+  // #burgerが表示される
+  $('#close').on('click', function(){
+    $('#nav-open').css('display','none');
+    $('#burger').css('display','flex');
+  });
+});
+
+
+ 
